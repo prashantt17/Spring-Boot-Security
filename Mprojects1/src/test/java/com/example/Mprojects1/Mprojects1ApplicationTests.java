f4ba4979defc975756609da650a457a406bba3a3
Prashant Kumar <prashantlucknow11@gmail.com>
2020-11-11 23:16:06 +0530

    Initial Commit

---
+++ Mprojects1/src/test/java/com/example/Mprojects1/Mprojects1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mprojects1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mprojects1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
